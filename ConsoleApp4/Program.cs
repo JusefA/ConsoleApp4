b38062e6564ac00802f8b23a593bf820921c087d
Jusef Al-itawa <jusef03@gmail.com>
2023-01-04 15:42:46 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿//your mum lol
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ifthisdoesnotworkillkillu
+{
+    class Gaetest2
+    {
+        static string Yourmumgay(int length)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(validChars[rnd.Next(validChars.Length)]);
+            }
+            return res.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+           
+            int length = 10;
+
+            string pass = Yourmumgay(length);
+            Console.Write(pass);
+        }
+    }
+}
